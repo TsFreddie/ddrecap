18f5621b2a70956f68a83f0cbf55b7651507c7da
TsFreddie <whatis@tsdo.in>
2025-12-23 20:01:55 +0800

    fix warning

--- src/routes/guard/+page.svelte
+++ src/routes/guard/+page.svelte
@@ -33,6 +33,7 @@
 						style="-webkit-text-security: disc; -moz-text-security: disc; text-security: disc;"
 					/>
 				</div>
+				<!-- svelte-ignore a11y_consider_explicit_label -->
 				<button
 					type="submit"
 					class="group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
